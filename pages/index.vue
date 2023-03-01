27a8b92d5552f32aeec1679a2e800ea0a2ac6d6d
qtsunami <xuyanlu0424@163.com>
2023-03-01 16:07:47 +0800

    pages edit

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="flex bg-gray-200 h-screen px-80 py-5 justify-between">
-    <div class="w-8/12 bg-blue-400">
+  <div class="flex h-screen px-80 py-5 justify-between">
+    <div class="w-8/12">
       <div class="article w-[100%]">
-        <div class="p-4">
+        <!-- <div class="p-4">
           <img class="w-80" src="https://krs-oversea.oss-cn-beijing.aliyuncs.com/2023/02/a916e05fe46ea2078b54dcf46e70f0c7.jpg" />
           
+        </div> -->
+
+        <div class="w-[100%] bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+          <div class="md:flex">
+            <div class="md:flex-shrink-0">
+              <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://krs-oversea.oss-cn-beijing.aliyuncs.com/2023/02/a916e05fe46ea2078b54dcf46e70f0c7.jpg" alt="Man looking at item at a store">
+            </div>
+            <div class="p-8">
+              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+              <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+              <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+            </div>
+          </div>
         </div>
       </div>
       <div class="paganaor">
-        分页
+        分页分页分页分页分页分页分页分页分页分页分页分页分页分页分页分页分页分页分页分页分页分页
       </div>
       
     </div>
