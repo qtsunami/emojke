471fcffa4532169ab902dd89344f87e84e23b911
qtsunami <simple.xull@gmail.com>
2023-03-05 23:54:21 +0800

    element ui

--- components/EFooter.vue
+++ components/EFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="text-center z-40">
-    Create by Emojke
+  <footer class="w-[100%] justify-between p-10">
+    <div class="w-[100%] text-center">Create by Emojke</div>
   </footer>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxtjs/tailwindcss"
+    "@nuxtjs/tailwindcss",
+    "@element-plus/nuxt",
   ],
   css: [
     "@/assets/css/tailwind.css",
