6b232427e19a9fc1ede8d97c4833e1001dc9d4d5
qtsunami <xuyanlu0424@163.com>
2023-03-10 14:29:52 +0800

    list responsvible

--- pages/list.vue
+++ pages/list.vue
@@ -1,58 +1,58 @@
 <template>
   <div class="max-w-7xl mx-auto py-3.5 px-6 md:flex justify-between items-center">
     <div class="w-full md:w-3/4 md:mr-7 border">
-      <div class="article mt-4 bg-gray-50 py-2 rounded">
+      <div class="article mt-4 bg-gray-50 py-2 rounded pb-4">
         <div class="w-full md:h-60">
           <img class="w-full h-full" src="https://krs-oversea.oss-cn-beijing.aliyuncs.com/2023/02/a916e05fe46ea2078b54dcf46e70f0c7.jpg" alt="Man looking at item at a store">
         </div>
-        <div class="w-full px-5 md:h-40">
+        <div class="w-full px-5 md:h-40 md:overflow-hidden">
           <div class="py-1 px-2">
             <a href="#" class="text-xl font-bold hover:text-blue-500">不同方面浅浅认识一下 Synchronized</a>
           </div>
-          <div>
-            <ul class="flex text-sm text-gray-400">
+          <div class="py-2">
+            <ul class="flex text-xs text-gray-400">
               <li class="px-2"><i class="bi bi-calendar3"></i> 2022-11-20 12:12:34</li>
               <li class="px-2"><i class="bi bi-person-fill"></i> 西梧RT</li>
               <li class="px-2"><i class="bi bi-eye-fill"></i> 200</li>
             </ul>
           </div>
-          <div class="px-2 mt-2">
+          <div class="px-2">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa nt ium dolorem que laudantium totam rem aperiam.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa nt ium dolorem que laudantium totam rem aperiam.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </div>
         </div>
       </div>
 
-      <div class="article mt-4 bg-gray-50 py-2 rounded">
-        <div class="w-full px-5 md:h-40">
+      <div class="article mt-4 bg-gray-50 py-2 rounded pb-4">
+        <div class="w-full px-5 md:h-40 md:overflow-hidden">
           <div class="py-1 px-2">
             <a href="#" class="text-xl font-bold hover:text-blue-500">不同方面浅浅认识一下 Synchronized</a>
           </div>
-          <div>
-            <ul class="flex text-sm text-gray-400">
+          <div class="py-2">
+            <ul class="flex text-xs text-gray-400">
               <li class="px-2"><i class="bi bi-calendar3"></i> 2022-11-20 12:12:34</li>
               <li class="px-2"><i class="bi bi-person-fill"></i> 西梧RT</li>
               <li class="px-2"><i class="bi bi-eye-fill"></i> 200</li>
             </ul>
           </div>
-          <div class="px-2 mt-2">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa nt ium dolorem que laudantium totam rem aperiam.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa nt ium dolorem que laudantium totam rem aperiam.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <div class="px-2">
+            Sed ut perspiciatis unde totam rem aperiam.Lormet, consectetur adipiscing elit.
           </div>
         </div>
       </div>
 
-      <div class="article mt-4 bg-gray-50 py-2 rounded">
-        <div class="w-full px-5 md:h-40">
+      <div class="article mt-4 bg-gray-50 py-2 rounded pb-4">
+        <div class="w-full px-5 md:h-40 md:overflow-hidden">
           <div class="py-1 px-2">
             <a href="#" class="text-xl font-bold hover:text-blue-500">不同方面浅浅认识一下 Synchronized</a>
           </div>
-          <div>
-            <ul class="flex text-sm text-gray-400">
+          <div class="py-2">
+            <ul class="flex text-xs text-gray-400">
               <li class="px-2"><i class="bi bi-calendar3"></i> 2022-11-20 12:12:34</li>
               <li class="px-2"><i class="bi bi-person-fill"></i> 西梧RT</li>
               <li class="px-2"><i class="bi bi-eye-fill"></i> 200</li>
             </ul>
           </div>
-          <div class="px-2 mt-2">
+          <div class="px-2">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa nt ium dolorem que laudantium totam rem aperiam.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa nt ium dolorem que laudantium totam rem aperiam.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </div>
         </div>
