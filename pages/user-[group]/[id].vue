4b11f2d7f6fb39ee481958dfdbd414a285d10502
qtsunami <simple.xull@gmail.com>
2023-03-24 00:05:15 +0800

    server api

--- pages/user-[group]/[id].vue
+++ pages/user-[group]/[id].vue
@@ -1,8 +1,13 @@
 <template>
   <p>{{ $route.params.group }} - {{ $route.params.id }}</p>
+  <p>{{ res }}</p>
 </template>
 
 <script setup>
   const route = useRoute()
   console.log(route.params.group, route.params.id)
+
+  // const { data: api } = await useFetch('/api/hello')
+  // console.log(api)
+  const { data: res } = await useFetch('/api/hello')
 </script>
